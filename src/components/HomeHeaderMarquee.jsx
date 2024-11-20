0b79de43b2d0b841595f77bb9d94b63ef0542b75
shahedul-alam <shaheedalamcontact@gmail.com>
2024-11-21 00:41:16 +0600

    breaking news added in the marquee and clicking it takes to the full news page

--- src/components/HomeHeaderMarquee.jsx
+++ src/components/HomeHeaderMarquee.jsx
@@ -1,18 +1,27 @@
+import { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 const HomeHeaderMarquee = () => {
+  const [breakingNews, setBreakingNews] = useState([]);
+
+  useEffect(() => {
+    fetch('https://openapi.programming-hero.com/api/news/category/01')
+    .then(res => res.json())
+    .then(data => setBreakingNews(data.data));
+  }, []);
+
   return (
     <section className="bg-gray-100 flex mb-5">
       <div className="bg-red-600 flex items-center rounded-l-md">
         <p className="text-white text-xl font-medium px-4">Latest</p>
       </div>
       <Marquee pauseOnHover={true} className="text-lg font-semibold py-2">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam,
-          quisquam iure. Rerum exercitationem inventore aperiam placeat
-          explicabo nulla quia ea blanditiis eveniet vero, obcaecati ullam
-          voluptate, doloribus, natus perspiciatis incidunt!
-        </p>
+        <div>
+          {
+            breakingNews.map(news => <Link to={`/news/${news._id}`}> {news.title} |</Link>)
+          }
+        </div>
       </Marquee>
     </section>
   );
